78479c88b9853fecd6716f4c3b79da1f1254c62e
Rezky Maulana <rezkymaulanaa06@gmail.com>
2025-12-12 00:47:03 +0700

    chore: update shared files for User and Academic Year features
    - Update AppServiceProvider with repository bindings
    - Update web-admin.php with routes for Users and Academic Years
    - Update verticalMenu.json with new menu items
    - Update MVP_FEATURES.md with completed features

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Repositories\AcademicYearRepository;
+use App\Repositories\Contracts\AcademicYearRepositoryInterface;
 use App\Repositories\Contracts\UserRepositoryInterface;
 use App\Repositories\Contracts\StudentRepositoryInterface;
 use App\Repositories\Contracts\TeacherRepositoryInterface;
@@ -21,6 +23,7 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
         $this->app->bind(StudentRepositoryInterface::class, StudentRepository::class);
         $this->app->bind(TeacherRepositoryInterface::class, TeacherRepository::class);
+        $this->app->bind(AcademicYearRepositoryInterface::class, AcademicYearRepository::class);
     }
 
     /**
